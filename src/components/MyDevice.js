36d36e8eeeecbb2b1bfd598ea7eae167f186e768
nishant0811 <renegade.plays03@gmail.com>
2022-06-08 08:54:33 +0530

    myDevice structure made

--- src/components/MyDevice.js
+++ src/components/MyDevice.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React , {useState} from 'react'
 
+import {Link , useNavigate} from 'react-router-dom'
+import Navigation from "./Navigation"
+
+import "./css/myDevice.css"
 
 const MyDevice = (props) => {
   return (
     <div>
-      <h1>
-        Hello
-      </h1>
+      <Navigation />
+
+    <div className="device__list__container">
+      <div className="device__container">
+        <div className="left__section">
+          <div>
+            <p className="device__name">Device Name  <span> <img className="editName" src="./assets/edit.png"></img> </span> </p>
+            <p>Device Id</p>
+          </div>
+        </div>
+        <div className="middle__section">
+          <div className="status">
+            <div className="item">
+              <img src="./assets/battery.png"></img>
+              <p>Battery Information</p>
+            </div>
+            <div className="item">
+              <img src="./assets/clock.png"></img>
+              <p>Speed</p>
+            </div>
+            <div className="item">
+              <img src="./assets/wifi.png"></img>
+              <p>Signal Strength</p>
+            </div>
+            <div className="item">
+              <img src="./assets/location.png"></img>
+              <p>Location</p>
+            </div>
+          </div>
+        </div>
+        <div className="right__section">
+          <div className="sub__status">
+            <p>Overdue</p>
+          </div>
+          <button className="action__button">
+            Renew
+          </button>
+        </div>
+
+        <div className='close'>
+          <p>
+            X
+          </p>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
