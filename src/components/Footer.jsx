ba7f7c63e9db2593b349c838aac8a26cdf4a75c1
amardesai1 <amardesai1@outlook.com>
2022-07-04 10:44:26 +0100

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
           <Link to="/privacy" className=" text-sm text-white">
             Privacy
           </Link>
-          <Link to="/terms" className=" text-sm text-white">
+          <Link to="/tos" className=" text-sm text-white">
             Terms
           </Link>
         </div>
