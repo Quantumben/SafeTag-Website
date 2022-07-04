b57faff2cb7ec6d1b2b0718edba43ff578427ed5
amardesai1 <amardesai1@outlook.com>
2022-07-04 10:05:21 +0100

    privacy & tos formatting

--- src/components/Tos.jsx
+++ src/components/Tos.jsx
@@ -12,51 +12,58 @@ const Tos = () => {
 
       <div className=" my-10 w-full px-3 lg:px-0 flex items-center justify-center">
         <div className=" w-full lg:w-1/3 new_class p-4">
-          <h6 className=" font-semibold text-xs w-full text-center"> 
-            The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD.<br /> 
-            These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case. <br />
-          </h6>
           <p className=" pb-2 text-xs font-medium text-center">
-          1. Commencement of service <br />
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD.          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case.
+          </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 1. Commencement of service </h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            Products purchase directly through the SafeTag store are delivered Ex Works. Delivery to the customer will be provided at the risk and expense of the customer by a transport method chosen by SafeTag. 
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
-          2. Delivery
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag reserves the right to make deliveries in instalments unless agreed otherwise. 
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
-          3. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            Products purchase directly through the SafeTag store are delivered Ex Works. Delivery to the customer will be provided at the risk and expense of the customer by a transport method chosen by SafeTag. 
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
-          4. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
-          </p>
+            Shipping delays caused by SafeTag or any of it’s service providers resulting in products that do not ship on the promised date, are accepted by the customer and no claims can be made by the customer as a result of this delay. The customer is entitled to a revised delivery date without delay.           </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 2. Delivery </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          5. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
-          </p>
+            Customers receiving cross-border goods take full responsibility and accept the cost for the clearance of these goods into their respective country.            </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 3. Payment </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          6. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 4. Alterations </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          7. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 5. Service Provision </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          8. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 6. Warranty Information </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          9. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 7. Limitation of liability </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-          10. Commencement of service
-          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
-
+          <h5 className=" font-semibold text-xs w-full text-center"> 8. Intellectual Property </h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 9. Resale </h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <h5 className=" font-semibold text-xs w-full text-center"> 10. Governing law, Disputes and Jurisdiction </h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
         </div>
       </div>
       <Footer2/>
