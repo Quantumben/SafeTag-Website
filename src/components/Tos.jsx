46faaa0346df2d0077dafdd2f35c731904a82eda
amardesai1 <amardesai1@outlook.com>
2022-07-04 09:55:07 +0100

    privacy & tos formatting

--- src/components/Tos.jsx
+++ src/components/Tos.jsx
@@ -13,11 +13,11 @@ const Tos = () => {
       <div className=" my-10 w-full px-3 lg:px-0 flex items-center justify-center">
         <div className=" w-full lg:w-1/3 new_class p-4">
           <h6 className=" font-semibold text-xs w-full text-center"> 
-            The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD.<br> 
-            These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case. <br> 
+            The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD.<br /> 
+            These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case. <br />
           </h6>
           <p className=" pb-2 text-xs font-medium text-center">
-          1. Commencement of service
+          1. Commencement of service <br />
           SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
