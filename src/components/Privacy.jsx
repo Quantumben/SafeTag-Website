b24eaa8c9ab597d18cf8a0865ebab205541dce93
amardesai1 <amardesai1@outlook.com>
2022-07-04 09:39:53 +0100

    privacy & tos

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -7,52 +7,32 @@ const Privacy = () => {
     <div>
       <Navigation />
       <h1 className=" mt-10 text-5xl font-bold w-full text-center">
-        Privacy Policy
+        SafeTag Privacy Policy
       </h1>
 
       <div className=" my-10 w-full px-3 lg:px-0 flex items-center justify-center">
         <div className=" w-full lg:w-1/3 new_class p-4">
-          <h6 className=" font-semibold text-xs w-full text-center"> What is Lorem Ipsum? </h6>
+          <h5 className=" font-semibold text-xs w-full text-center"> Your personal data </h5>
 
           <p className=" pb-2 text-xs font-medium text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Here at SafeTag we are dedicated to protecting the privacy of our customers. In accordance with EU’s General Data Protection Regulation (GDPR), we use the data collected about our customers lawfully and only to process their purchases and provide the best quality service possible.
+            Upon logging in to www.safetagtracking.com, several cookies will be created to remember your login information to speed up and ease the login process. This data is encrypted and protected.
+            SafeTag stores your username/email and password for login purposes. SafeTag also stores your name, if you enter this during your registration.
+            We do not store bank/credit card details and all payments are encrypted and processed using Stripe, available here: https://stripe.com/gb . Your information is not shared with any third parties. All payment information is handled via Stripe and SafeTag will not hold or receive any of this 
           </p>
-          <h6 className=" font-semibold text-xs w-full text-center">Why do we use it?</h6>
+          <h5 className=" font-semibold text-xs w-full text-center">Geo-location Data</h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy. Various versions have evolved over the
-            years, sometimes by accident, sometimes on purpose (injected humour
-            and the like).
+            Geo-location data collected by SafeTag branded GPS tracking equipment is saved for the last three months and is accessible in-app or via the website. This data is encrypted and secured within MongoDB Atlas database infrastructure, your personal 3-month location history is only accessible from your account, however the use of non-personal aggregated data may be shared with other parties for the purpose of analysis and to improve our service. We will not use or share this data for any other purpose.
+            Data from both your smartphone(app) and computer(website) is used to determine your geographic location for the sole purpose of displaying this information to you via each of these channels. You will have authorised this in-app and on our website. Also, we may use it for statistics analysis and share that non-personal aggregated data with other parties. We will not use or share this data for any other purpose.
+            If you do not want us to use your location for the purposes set forth above, you should switch off location permissions for the SafeTag Mobile Application or Website in your phone or web browser settings. This will no longer display the location of your smartphone. 
           </p>
-
-          <h6 className=" font-semibold text-xs w-full text-center">Where does it come from?</h6>
+          <h5 className=" font-semibold text-xs w-full text-center">E-mail marketing practices</h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            Lorem Ipsum is not simply random text. It has roots in a piece of
-            classical Latin literature from 45 BC, making it over 2000 years
-            old. Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the cites
-            of the word in classical literature, discovered the undoubtable
-            source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de
-            Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-            Cicero, written in 45 BC. This book is a treatise on the theory of
-            ethics, very popular during the Renaissance. The first line of Lorem
-            Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-            1.10.32.
+            You may opt-out from any email marketing by using the unsubscribe links at the bottom of the marketing emails or in your account preferences.
+          </p>
+          <h5 className=" font-semibold text-xs w-full text-center">Contacting Us</h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            When you contact us via contact form or e-mail, your personal data is collected. The data collected is shown as listed in the respective contact form. This data is stored and used exclusively for the purpose of responding to your request or for establishing contact and for applying technical assistance.  
           </p>
         </div>
       </div>
--- src/components/Tos.jsx
+++ src/components/Tos.jsx
@@ -7,53 +7,56 @@ const Tos = () => {
     <div>
       <Navigation />
       <h1 className=" mt-10 text-5xl font-bold w-full text-center">
-      Terms of Service
+      SafeTag Terms and Conditions of Sale and Service
       </h1>
 
       <div className=" my-10 w-full px-3 lg:px-0 flex items-center justify-center">
         <div className=" w-full lg:w-1/3 new_class p-4">
-          <h6 className=" font-semibold text-xs w-full text-center"> What is Lorem Ipsum? </h6>
+          <h6 className=" font-semibold text-xs w-full text-center"> 
+            The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD. 
+            These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case. 
+          </h6>
+          <p className=" pb-2 text-xs font-medium text-center">
+          1. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          2. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          3. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          4. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          5. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          6. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          7. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          8. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          9. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+          10. Commencement of service
+          SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+          </p>
 
-          <p className=" pb-2 text-xs font-medium text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <h6 className=" font-semibold text-xs w-full text-center">Why do we use it?</h6>
-          <p className=" pb-2 text-xs font-medium text-center">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy. Various versions have evolved over the
-            years, sometimes by accident, sometimes on purpose (injected humour
-            and the like).
-          </p>
-
-          <h6 className=" font-semibold text-xs w-full text-center">Where does it come from?</h6>
-          <p className=" pb-2 text-xs font-medium text-center">
-            Lorem Ipsum is not simply random text. It has roots in a piece of
-            classical Latin literature from 45 BC, making it over 2000 years
-            old. Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the cites
-            of the word in classical literature, discovered the undoubtable
-            source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de
-            Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-            Cicero, written in 45 BC. This book is a treatise on the theory of
-            ethics, very popular during the Renaissance. The first line of Lorem
-            Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-            1.10.32.
-          </p>
         </div>
       </div>
       <Footer2/>
