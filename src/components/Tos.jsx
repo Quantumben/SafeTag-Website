9b3cf0c4019230f4b970a770e1d37a3bf17174a9
amardesai1 <amardesai1@outlook.com>
2022-07-04 10:17:47 +0100

    privacy & tos formatting

--- src/components/Tos.jsx
+++ src/components/Tos.jsx
@@ -12,12 +12,18 @@ const Tos = () => {
 
       <div className=" my-10 w-full px-3 lg:px-0 flex items-center justify-center">
         <div className=" w-full lg:w-1/3 new_class p-4">
+
           <p className=" pb-2 text-xs font-medium text-center">
             The terms and conditions of sale & service apply to all sales, deliveries, services and products supplied and sold by SafeTag or any of it’s affiliates, these affiliates will be any entity directed by DownRiver LTD.          </p>
           <p className=" pb-2 text-xs font-medium text-center">
             These general terms and conditions of sale & service may be overruled by product or service specific terms and conditions of sale & service, these will be made available and apparent to you if this is this case.
           </p>
+
           <h5 className=" font-semibold text-xs w-full text-center"> 1. Commencement of service </h5>
+          <p className=" pb-2 text-xs font-medium text-center">
+            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.           </p>
+          
+          <h5 className=" font-semibold text-xs w-full text-center"> 2. Delivery </h5>
           <p className=" pb-2 text-xs font-medium text-center">
             Products purchase directly through the SafeTag store are delivered Ex Works. Delivery to the customer will be provided at the risk and expense of the customer by a transport method chosen by SafeTag. 
           </p>
@@ -25,44 +31,71 @@ const Tos = () => {
             SafeTag reserves the right to make deliveries in instalments unless agreed otherwise. 
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
-            Products purchase directly through the SafeTag store are delivered Ex Works. Delivery to the customer will be provided at the risk and expense of the customer by a transport method chosen by SafeTag. 
+            Shipping delays caused by SafeTag or any of it’s service providers resulting in products that do not ship on the promised date, are accepted by the customer and no claims can be made by the customer as a result of this delay. The customer is entitled to a revised delivery date without delay.           
           </p>
           <p className=" pb-2 text-xs font-medium text-center">
-            Shipping delays caused by SafeTag or any of it’s service providers resulting in products that do not ship on the promised date, are accepted by the customer and no claims can be made by the customer as a result of this delay. The customer is entitled to a revised delivery date without delay.           </p>
-          <h5 className=" font-semibold text-xs w-full text-center"> 2. Delivery </h5>
-          <p className=" pb-2 text-xs font-medium text-center">
-            Customers receiving cross-border goods take full responsibility and accept the cost for the clearance of these goods into their respective country.            </p>
+            Customers receiving cross-border goods take full responsibility and accept the cost for the clearance of these goods into their respective country.
+          </p>            
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 3. Payment </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag reserves the right to cancel any service or any order that it desires for any reasonable reason with a full or pro-rata refund to the customer. 
           </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            Business offers made by SafeTag have a duration of 30 days before they are automatically rescinded and are subject to the terms and conditions laid out in this document. 
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            Unless otherwise arranged the full payment amount of an invoice is due at most 14 days after it is served. 
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            SafeTag reserves the right to adjust accepted prices for non-delivered products in the event of changes in the rates of exchange, costs of materials, a sub-supplier's price increases, wages, or similar conditions over which SafeTag has no or limited control. 
+          </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 4. Alterations </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            SafeTag reserves the right to, without notice or approval from the customer to make alterations to a product that do not affect the agreed specifications or the products, form fit or function.
           </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 5. Service Provision </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            The service provided by SafeTag products is subject to receipt of payment before the commencement of the service. Without payment SafeTag reserves the right to cancel any ongoing service period either indefinitely or until payment is made for the period in which that payment was not received and for any future periods.
           </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            GPS Trackers sold by SafeTag with SafeTag branding are sold-as fixed units, the customer is not permitted to alter or edit SafeTag trackers in any way. In any violation of this policy SafeTag reserves the right to cancel its service effectively immediately, the customer takes full responsibility for their own health and safety and is not entitled to any refund.
+          </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 6. Warranty Information </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            The warranty period of SafeTag branded trackers is subject to the warranty agreement accepted by the customer at the PoS. This will be made evident to the customer either as one of the products USPs or via the product information section of the customers product on the website.
           </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            Any alteration of SafeTags trackers by the customer will result in the immediate cancellation of their warranty agreement, and SafeTag is not liable for any damages caused by the altered unit or damages caused to the unit itself.
+          </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 7. Limitation of liability </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            Upon purchase of a product the customer accepts full liability of any indirect, consequential, punitive or other damages or losses caused by faulty products, delayed delivery, breaches of warranty, recalled products or otherwise. This includes but isn’t limited to the loss of goods, production, profit or goodwill.
           </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 8. Intellectual Property </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            Goods sold by SafeTag that are delivered with embedded software are of full ownership of DownRiver Ltd. The customer obtains a non-exclusive licence to use this software subject to the specific purpose of the product. Aside, the customer obtains no rights or ownership of any licence, patent, copyright or trademark connected to the product. The customer does not own any right to the source code of embedded or non-embedded service software. 
           </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 9. Resale </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            For international safety purposes, SafeTag reserves the right to be the only supplier of SafeTag branded goods and services, customers are not allowed to use or resell it’s products for purposes other than set-out by the device.
           </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            If the resale of devices is suspected, SafeTag reserves the rights to cancel any service provided for the device in question, and the customer who sold the device is liable for damages, either emotional or physical caused by this sale.
+          </p>
+          
           <h5 className=" font-semibold text-xs w-full text-center"> 10. Governing law, Disputes and Jurisdiction </h5>
           <p className=" pb-2 text-xs font-medium text-center">
-            SafeTag accepts confirmation of an order when written, including electronic, acceptance by a customers purchase order has been received by SafeTag and accepts the terms & conditions laid out by this document.
+            Any dispute between the parties concerning or arising from (a) a purchase of a Product or (b) any damages, are subject to decision under the law of the United Kingdom, with the area of jurisdiction being London, England. 
+          </p>
+          <p className=" pb-2 text-xs font-medium text-center">
+            Any such dispute which the parties themselves are unable to resolve can, at the choice of SafeTag, be referred to and settled by arbitration in London, England. 
           </p>
         </div>
       </div>
